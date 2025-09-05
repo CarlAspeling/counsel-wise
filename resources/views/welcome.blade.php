4ecdddbe5e564a1819c453f85a13fcbf761c9489
Carl Aspeling <carlaspeling@gmail.com>
2025-09-05 11:39:33 +0200

    Set up frontend development environment - various views, partials and components set up (need to check this one out carefully still)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+</head>
+<body>
+<div>
+    <h1>Welcome to Laravel</h1>
+    <a href="/dashboard">Dashboard</a>
+</div>
+</body>
+</html>
