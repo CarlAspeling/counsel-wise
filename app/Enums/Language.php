6bdbfe88e59f4d1fff0f07cefecf4cc6f83a1b51
Carl Aspeling <carlaspeling@gmail.com>
2025-09-11 12:02:55 +0200

    18 - Add foundational enums for user account management and demographics

---
+++ app/Enums/Language.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+enum Language: string
+{
+    case English = 'english';
+    case Afrikaans = 'afrikaans';
+    case Zulu = 'zulu';
+    case Xhosa = 'xhosa';
+    case Sotho = 'sotho';
+    case Tswana = 'tswana';
+    case Venda = 'venda';
+    case Tsonga = 'tsonga';
+    case Ndebele = 'ndebele';
+    case Swati = 'swati';
+    case Pedi = 'pedi';
+}
